ef157c4bcd54beea7ec10a8f95fb5d90f143534a
Colum Foskin <columfoskin@gmail.com>
2016-11-12 15:27:32 +0000

    added leadsender controller

---
+++ app/controllers/LeadSender.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const agSender = require('unifiedpush-node-sender');
+const settings = require('../../config/config.js');
+
+
+exports.sendLeads =  () => {
+}
+
+exports.sendBroadcast =  () => {
+}
+
+var getActivePushConfig = () => {
+    
+}
