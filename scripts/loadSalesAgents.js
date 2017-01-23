aa478e64ee37d0f55b44066a16230db1c74669f6
Colum Foskin <columfoskin@gmail.com>
2017-01-23 15:43:58 +0000

    adding in new folders

---
+++ scripts/loadSalesAgents.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const config = require('../config/config');
+const db = require('mongoose').connect(config.database).connection;
+const salesAgents = require('../resources/fixtures').salesAgents;
+const SalesAgent = require('../app/models/SalesAgent');
+
+db.on('error', console.error.bind(console, 'Error connecting to Mongo Database:'));
+db.once('open', function() {
+    console.log('Connected to Mongo Database');
+    salesAgents.forEach((salesAgent) => {
+    	   salesAgent.id = Math.floor((Math.random() * 4732981560546796792) + 1).toString();
+    });
+    SalesAgent.collection.insert(salesAgents, () => {
+        console.log('Users added to Database');
+    });
+    db.close();
+});
