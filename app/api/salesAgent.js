ba0d8ae50a510579e6cbdb1f71be776fbe0a1420
Colum Foskin <columfoskin@gmail.com>
2017-03-07 21:17:07 +0000

    added geolocation handling for agents

--- app/api/salesAgent.js
+++ app/api/salesAgent.js
@@ -1,20 +1,39 @@
 'use strict';
 
 const SalesAgent = require('../models/SalesAgent');
+var NodeGeocoder = require('node-geocoder');
+var geocoder = NodeGeocoder();
 
 exports.create = (req, res) => {
     const salesAgent = new SalesAgent(req.body);
-    salesAgent.id = Date.now().toString();
-    salesAgent.save()
-        .then(newSalesAgent => {
-            return res.status(201).json(newSalesAgent);
+    let coordinates = [[], []];
+    //setting the cordinates on the 2d array for geospatial query
+    coordinates[0] = salesAgent.latitude;
+    coordinates[1] = salesAgent.longitude;
+    //get the city using the lat - long pairs
+    geocoder.reverse({ lat: coordinates[0], lon: coordinates[1] })
+        .then( place => {
+            //update the sales agents location to the city returned
+            salesAgent.location = place[0].city;
+            salesAgent.coordinates = coordinates;
+            salesAgent.id = Date.now().toString();
+            return salesAgent.save()
+                .then(newSalesAgent => {
+                    return res.status(201).json(newSalesAgent);
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        message: 'Error creating sales agent',
+                        error: err
+                    });
+                });
         })
         .catch(err => {
-            return res.status(500).json({
-                message: 'Error creating sales agent',
+            return res.status(404).json({
+                message: 'Error - no location found for lat/long pair - sales agent not created',
                 error: err
             });
-        })
+        });
 };
 
 exports.getOne = (req, res) => {
@@ -100,6 +119,24 @@ exports.searchAgents = (req, res) => {
 };
 
 exports.searchAgentsInRange = (req, res) => {
-    //descoped from sprint due to android feature missing
-    return res.status(200).json([]);
+    var limit = req.query.limit || 10;
+    var radius = req.query.radius;
+    radius /= 111.12;
+    var coords = [];
+    coords[0] = req.query.latitude;
+    coords[1] = req.query.longitude;
+    console.log(coords);
+    // find an agent in the radius of the circle on the map
+    SalesAgent.find({
+        coordinates: {
+            $near: coords,
+            $maxDistance: radius
+        }
+    }).limit(limit).exec(function(err, salesAgents) {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        res.status(200).json(salesAgents);
+    });
+
 };
