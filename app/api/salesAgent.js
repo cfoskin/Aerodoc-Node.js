b3a9f89c9408ad236dcdd65cc1e9b923c57f46a1
Colum Foskin <columfoskin@gmail.com>
2017-03-16 10:29:21 +0000

    sonar fixes

--- app/api/salesAgent.js
+++ app/api/salesAgent.js
@@ -44,7 +44,7 @@ exports.getAll = (req, res) => {
 exports.update = (req, res) => {
     SalesAgent.findOne({ id: req.params.id })
         .then(salesAgent => {
-            let coordinates = [
+            const coordinates = [
                 [],
                 []
             ];
